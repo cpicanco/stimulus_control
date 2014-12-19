a1e7beb6eb42319dcc7c622d88a05cc28054a15b
cpicanco <cpicanco@ufpa.br>
2014-12-19 06:31:02 -0300

    Fixing Licenses.

--- gettimestamp.py
+++ gettimestamp.py
@@ -1,24 +1,13 @@
 '''
-//
-// Validation Project (PCRF) - Stimulus Control App
-// Copyright (C) 2014,  Carlos Rafael Fernandes Picanco, cpicanco@ufpa.br
-//
-// This file is part of Validation Project (PCRF).
-//
-// Validation Project (PCRF) is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Validation Project (PCRF) is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Validation Project (PCRF).  If not, see <http://www.gnu.org/licenses/>.
-//
+(*)~----------------------------------------------------------------------------------
 
+ Validation Project (PCRF) - Stimulus Control App
+ Copyright (C) 2014,  Carlos Rafael Fernandes Picanco, cpicanco@ufpa.br
+
+ Distributed under the terms of the CC BY-NC-SA License.
+ License details are in [http://creativecommons.org/licenses/by-nc-sa/3.0/].
+
+----------------------------------------------------------------------------------~(*)
 '''
 
 import sys
