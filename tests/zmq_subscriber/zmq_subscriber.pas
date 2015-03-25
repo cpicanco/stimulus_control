3fa701a5dac9a2ba07cad39d0482f430dd50e539
cpicanco <cpicanco@ufpa.br>
2015-03-25 20:12:45 -0300

    testing some new build/compile directives

--- tests/zmq_subscriber/zmq_subscriber.pas
+++ tests/zmq_subscriber/zmq_subscriber.pas
@@ -22,26 +22,33 @@ end;
 
 const
 	UPDATE_COUNT = 5000;
-
+	address = '10.42.0.1:5020';
 var
 	context : TZMQContext;
 	subscriber : TZMQSocket;
 	aBegin : TTimeStamp;
-	i : integer;
+	major, minor, patch, i : integer;
 
 	message : Utf8String;
-	FTimestampsPath, address, filter : string;
+	FTimestampsPath : string;
 	FTimestampsFile : TextFile;
 
 begin
-
+	ZmqVersion(major, minor, patch);
+	Writeln( 'zmq_version():' + IntToStr(major) + '.' + IntToStr(minor) + '.' + IntToStr(patch) );
 	// Socket to talk to server
-	address := '127.0.0.1:5020';
-	Writeln ( 'Connecting to Pupil server...' + #32#40 + 'address:' + #32 + address + #32#41);
+	
 	context := TZMQContext.Create;
+	Writeln( 'creating generic context');
+
 	subscriber := Context.Socket( stSub );
+	Writeln( 'assigning context stSub' );
+	
 	subscriber.connect( 'tcp://' + address );
+	Writeln( 'connecting to:  tcp://' + address  );
+	
 	subscriber.subscribe( '' );
+	Writeln('subscribing with empty filter');
 
 	FTimestampsPath := '/home/rafael/git/validation_project/Participante1/Data/timestamps_teste';
 
@@ -56,6 +63,7 @@ begin
 		for i := 0 to UPDATE_COUNT - 1 do
 			begin
 				subscriber.recv( message );
+				Writeln('receiving messsage');
 				message := GetTimestampFromMessage(message);
 				Writeln(FTimestampsFile, message);
 				Writeln( message + #32 + IntToStr(DateTimeToTimeStamp(Now).Time - aBegin.Time));
@@ -63,7 +71,6 @@ begin
 	finally
 		CloseFile(FTimestampsFile);
 		subscriber.Free;
-		context.Free;
-		Writeln ('End')		
+		context.Free;		
 	end;
 end.
