d0424ff46120563637fab99c419bfebdd6bf3507
cpicanco <cpicanco@ufpa.br>
2015-05-15 15:55:34 -0300

    rewriting TMSG
    - FLimitedHold support
    - Refactoring
    - Preparing for multilanguage support (resourcestring)

--- units/trial_message.pas
+++ units/trial_message.pas
@@ -25,233 +25,173 @@ unit trial_message;
 
 interface
 
-uses Controls, Classes, ExtCtrls, SysUtils, StdCtrls,
-     Graphics, Forms, LCLIntf, LCLType
+uses  LCLIntf, LCLType, Controls,
+      Classes, SysUtils, StdCtrls
 
      , trial_abstract
+     , constants
      //, countermanager
      ;
 
 type
 
+  TDataSupport = record
+    //Latency : cardinal;
+    //Responses : integer;
+    TrialBegin : cardinal;
+    TrialEnd : cardinal;
+  end;
+
+  { TMSG }
+
   TMSG = class(TTrial)
   protected
-    FTrialInterval: Integer;
-    FFlagResp : Boolean;
-    FCanPassTrial : Boolean;
-    FTimerCsq: TTimer;
-    FMemo: TMemo;
-    FMemoPrompt: TLabel;
-    Ft : Cardinal;
-    FLat: Cardinal;
-    procedure Consequence(Sender: TObject);
+    FDataSupport : TDataSupport;
+    FResponseEnabled : Boolean;
+    FMemo : TMemo;
+    FMemoPrompt : TLabel;
     procedure EndTrial(Sender: TObject);
-    procedure SetTimerCsq;
-    procedure TimerCsqTimer(Sender: TObject);
-    procedure MemoEnter(Sender: TObject);
-    procedure KeyDown(var Key: Word; Shift: TShiftState); override;
+    procedure ThreadClock(Sender: TObject); override;
+    //procedure MemoEnter(Sender: TObject);
     procedure MemoClick(Sender: TObject);
-    procedure Click; override;
+    procedure KeyUp(var Key: Word; Shift: TShiftState); override;
+    //procedure Click; override;
     procedure WriteData(Sender: TObject); override;
     procedure StartTrial(Sender: TObject); override;
   public
     constructor Create(AOwner: TComponent); override;
     procedure Play(TestMode: Boolean; Correction : Boolean); override;
-    procedure DispenserPlusCall; override;
   end;
 
+resourcestring
+
+  MessageMemoPrompt1 = 'Pressione  o botão  para avançar.';
+
 implementation
-
-//uses fUnit6;
-
-procedure TMSG.Click;
-begin
-  FLat := GetTickCount;
-  inherited Click;
-
-
-  DataTicks := DataTicks +
-               FormatFloat('####,####',FLat - Ft) + #9 +
-               'BkgndClk' + #9 +
-               '-' + #9 +
-               '-' + #9 +
-               '-' + LineEnding + #9 + #9;
-
-  EndTrial(Self);
-end;
-
-procedure TMSG.Consequence(Sender: TObject);
-begin
-  if FFlagResp then
-    begin
-      FFlagResp:= False;
-      FLat := GetTickCount;
-      if FCanPassTrial then FTimerCsq.Enabled:= True else
-        begin
-          EndTrial(Sender);
-          Ft := GetTickCount;
-          FFlagResp:= True;
-        end;
-    end;
-end;
 
 constructor TMSG.Create(AOwner: TComponent);
 begin
   Inherited Create(AOwner);
 
-  Header := 'Mensagem' + #9 + 'Lat.Cmp.';
-
-  HeaderTicks:= 'Tempo_ms' + #9 +
-                 'Cmp.Tipo' + #9 +
-                 'FileName' + #9 +
-                 'Left....' + #9 +
-                 'Top.....';
-
-  FFlagResp := False;
-
-  FTimerCsq:= TTimer.Create(Self);
-  with FTimerCsq do
-    begin
-      Enabled:= False;
-      Interval:= 1;
-      OnTimer:= TimerCsqTimer;
-    end;
+  Header := 'Message' + #9 + '___Start' + #9 + 'Duration';
 
   FMemo := TMemo.Create(Self);
-  With FMemo do begin
-    Text := #0;
-    Parent := Self;
-    Font.Name := 'TimesNewRoman';
-    Font.Color := clWhite;
-    ReadOnly := True;
-    BorderStyle := bsNone;
-    Color := clBlack;
-    OnEnter := MemoEnter;
+  with FMemo do begin
     Alignment := taCenter;
+    BorderStyle := bsNone;
+    Font.Name := 'TimesNewRoman';
+    Parent := Self;
+    ReadOnly := True;
+    Cursor := -1;
+    //Text := #0;
+    //Font.Color := clWhite;
+    //Color := clBlack;
+    //OnEnter := MemoEnter;
     OnClick := MemoClick;
   end;
 
   FMemoPrompt := TLabel.Create(Self);
-  With FMemoPrompt do begin
-    Caption := 'Click com o Mouse ou Pressione  <<Control>> + <<Enter>>  para Avançar';
-    Parent := Self;
+  with FMemoPrompt do begin
+    Caption := MessageMemoPrompt1;
     Font.Name := 'TimesNewRoman';
     Font.Size := 14;
+    Parent := Self;
     OnClick := MemoClick;
   end;
 
-
-end;
-
-procedure TMSG.DispenserPlusCall;
-begin
-  //
-
-end;
-
-procedure TMSG.KeyDown(var Key: Word; Shift: TShiftState);
-begin
-  inherited KeyDown (Key, Shift);
-  If (ssCtrl in Shift) and (Key = 13) then EndTrial(Self);
+  Result := T_NONE;
+  IETConsequence := T_NONE;
+  Result := T_NONE;
 end;
 
 procedure TMSG.MemoClick(Sender: TObject);
 begin
-  FLat := GetTickCount;
-  DataTicks:= DataTicks +
-               FormatFloat('####,####',FLat - Ft) + #9 +
-               'MemoClk' + #9 +
-               '-' + #9 +
-               '-' + #9 +
-               '-' + LineEnding + #9 + #9;
-
-  EndTrial(Self);
+  if not (FLimitedHold > 0) then EndTrial(Sender);
 end;
 
-procedure TMSG.MemoEnter(Sender: TObject);
+procedure TMSG.KeyUp(var Key: Word; Shift: TShiftState);
 begin
-  //SetFocus;
+  inherited KeyUp(Key, Shift);
+  if not (FLimitedHold > 0) then
+    if (scCtrl in Shift) and (Key = 32) then EndTrial(Self);
 end;
 
 procedure TMSG.Play(TestMode: Boolean; Correction : Boolean);
-var H: Integer;
+var H : Integer;
 begin
   //FIscorrection := Correction; //Messages with corrections were not implemented yet
 
-  NextTrial:= '-1';
-  Color:= StrToIntDef(CfgTrial.SList.Values['BkGnd'], 0);
-  FCanPassTrial := StrToBoolDef(CfgTrial.SList.Values['AutoNxt'], True);
-  if FCanPassTrial = False then
+  // TCustomControl
+  Color := StrToIntDef(CfgTrial.SList.Values[_BkGnd], $FFFFFF);
+
+  if TestMode then Cursor := 0
+  else Cursor := StrToIntDef(CfgTrial.SList.Values[_Cursor], 0);
+
+  // TTrial
+  NextTrial := CfgTrial.SList.Values[_NextTrial];
+  FLimitedHold := StrToIntDef(CfgTrial.SList.Values[_LimitedHold], 0);
+
+  // Self TMemo
+  with FMemo do
     begin
-      FTrialInterval := StrToIntDef(CfgTrial.SList.Values['CustomNxtValue'], 10000);
-      SetTimerCsq;
+      Cursor := Self.Cursor;
+      Text := CfgTrial.SList.Values[_Msg];
+      Width := StrToIntDef(CfgTrial.SList.Values[_MsgWidth], 640);
+      Font.Size := StrToIntDef(CfgTrial.SList.Values[_MsgFontSize], 22);
+      Font.Color := StrToIntDef(CfgTrial.SList.Values[_MsgFontColor], $000000);
+      Color := StrToIntDef(CfgTrial.SList.Values[_BkGnd], Self.Color);
+
+      H := (Lines.Count + 2) * Font.Height * -1;
+      SetBounds((Self.Width - Width) div 2, (Self.Height - H) div 2, Width, H);
+
+      Parent := Self;
     end;
 
-  If TestMode then Cursor := 0
-  else Cursor := StrToIntDef(CfgTrial.SList.Values['Cursor'], 0);
-
-  FMemo.Text := CfgTrial.SList.Values['Msg'];
-  FMemo.Width := StrToIntDef(CfgTrial.SList.Values['MsgWidth'], 640);
-  FMemo.Font.Size := StrToIntDef(CfgTrial.SList.Values['MsgFontSize'], 28);
-  FMemo.Font.Color := StrToIntDef(CfgTrial.SList.Values['MsgFontColor'], clWhite);
-  FMemo.Color := StrToIntDef(CfgTrial.SList.Values['BkGnd'], clBlack);
-
-  H:= (FMemo.Lines.Count + 2) * FMemo.Font.Height * -1;
-  FMemo.SetBounds((Width-FMemo.Width)div 2, (Height-H)div 2, FMemo.Width, H);
-
-  FMemo.Parent:= Self;
-  If Cursor = 0 then FMemo.Cursor:= crArrow
-  else FMemo.Cursor:= Cursor;
-
-  FMemoPrompt.Visible:= StrToBoolDef(CfgTrial.SList.Values['Prompt'], False);
-  If FMemoPrompt.Visible then begin
-    FMemoPrompt.SetBounds((Width-FMemoPrompt.Width)div 2, (Height-FMemoPrompt.Height)-20, FMemoPrompt.Width, FMemoPrompt.Height);
-    FMemoPrompt.Font.Color:= FMemo.Font.Color;
-  end;
+  // Self TLabel
+  FMemoPrompt.Visible := StrToBoolDef(CfgTrial.SList.Values[_Prompt], False);
+  with FMemoPrompt do
+    if Visible then
+      begin
+        SetBounds((Self.Width - Width) div 2, (Self.Height - Height) - 20, Width, Height);
+        Font.Color:= FMemo.Font.Color;
+      end;
 
   StartTrial(Self);
 end;
 
-procedure TMSG.SetTimerCsq;
-begin
-  FTimerCsq.Interval := FTrialInterval;
-end;
-
 procedure TMSG.StartTrial(Sender: TObject);
 begin
+  FResponseEnabled := True;
+  Invalidate;
+  FDataSupport.TrialBegin := GetTickCount;
   inherited StartTrial(Sender);
-  if FCanPassTrial then FTimerCsq.Enabled:= False else FTimerCsq.Enabled:= True;
-  FFlagResp:= True;
-  Ft := GetTickCount;
-end;
-
-procedure TMSG.TimerCsqTimer(Sender: TObject);
-begin
-  FTimerCsq.Enabled:= False;
-  FLat := GetTickCount;
-  DataTicks:= DataTicks +
-               FormatFloat('####,####',FLat - Ft) + #9 +
-               'TimeCsq' + #9 +
-               '-' + #9 +
-               '-' + #9 +
-               '-' + LineEnding + #9 + #9;
-  FCanPassTrial := True;
-  EndTrial(Self);
 end;
 
 procedure TMSG.WriteData(Sender: TObject);
-var Lat_Cmp : string;
+var aStart, aDuration : string;
 begin
-  Lat_Cmp := FormatFloat('#####,###',FLat - Ft) + #9 + 'ms';
-  Data := FMemo.Lines.Text + #9 + Lat_Cmp;
+  aStart := FormatFloat('00000000;;00000000', FDataSupport.TrialBegin - TimeStart);;
+  aDuration := FormatFloat('00000000;;00000000', FDataSupport.TrialEnd - TimeStart);
+
+  Data := FMemo.Lines.Text + #9 + aStart + #9 + aDuration + Data;
   if Assigned(OnWriteTrialData) then OnWriteTrialData(Sender);
 end;
 
 procedure TMSG.EndTrial(Sender: TObject);
 begin
+  FResponseEnabled := False;
+  Hide;
+  FDataSupport.TrialEnd := GetTickCount;
+
   WriteData(Sender);
-  if FCanPassTrial then
-    if Assigned(OnEndTrial) then OnEndTrial(Sender);
+  //if Result = T_NONE then None(Sender);
+
+  if Assigned(OnEndTrial) then OnEndTrial(Sender);
+end;
+
+procedure TMSG.ThreadClock(Sender: TObject);
+begin
+  EndTrial(Sender);
 end;
 
 end.
