a4ff0349601c22efd8fe0e3b383bed2d32736bec
cpicanco <cpicanco@ufpa.br>
2015-08-31 22:29:34 -0300

    timing

--- tests/timing/form_main.pas
+++ tests/timing/form_main.pas
@@ -28,7 +28,7 @@ interface
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs
   , custom_timer
-  //, draw_methods
+  , draw_methods
   ;
 
 type
@@ -36,9 +36,12 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    procedure FormActivate(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormPaint(Sender: TObject);
   private
     FClock : TClockThread;
+    FCanDraw : Boolean;
     procedure OnClockTimer(Sender : TObject);
     procedure OnClockTerminate(Sender: TObject);
   public
@@ -54,25 +57,52 @@ implementation
 
 { TForm1 }
 
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  FCanDraw := False;
+  FClock := TClockThread.Create(True);
+  FClock.Interval := 1000;
+  FClock.OnTimer := @OnClockTimer;
+  FClock.OnTerminate := @OnClockTerminate;
+  FClock.Start;
+end;
+
 procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
   );
 begin
-  if Key = 32 then
+  if (Key = 32) then
      begin
-       FClock := TClockThread.Create(True);
-       FClock.OnTimer := @OnClockTimer;
-       FClock.OnTerminate := @OnClockTerminate;
+  	   FCanDraw := not FCanDraw;
+       Invalidate;
+     end;
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+var i : integer;
+begin
+  if FCanDraw then
+     begin
+       for i := 0 to 5 do
+     		 DrawCircle(Canvas, 10 + (122*i), 10, 120, True, 90, 2);
+
+       for i := 0 to 5 do
+     		 DrawCircle(Canvas, 10 + (122*i), 130, 120, True, 90, 2);
+
+       for i := 0 to 5 do
+     		 DrawCircle(Canvas, 10 + (122*i), 250, 120, True, 90, 2);
+
      end;
 end;
 
 procedure TForm1.OnClockTimer(Sender: TObject);
 begin
-  // do nothing
+  FCanDraw := not FCanDraw;
+  Invalidate;
 end;
 
 procedure TForm1.OnClockTerminate(Sender: TObject);
 begin
-
+ //
 end;
 
 end.
