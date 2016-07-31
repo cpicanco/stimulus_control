5bafa6fa1598e7375706cd353b7535ebe40e47b7
cpicanco <cpicanco@ufpa.br>
2016-07-31 01:08:42 -0300

    update headers, readme and removing old stuff

--- units/trial_feature_positive.pas
+++ units/trial_feature_positive.pas
@@ -1,24 +1,12 @@
-//
-// Validation Project (PCRF) - Stimulus Control App
-// Copyright (C) 2014-2016,  Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
-//
-// cpicanco@ufpa.br
-//
-// This file is part of Validation Project (PCRF).
-//
-// Validation Project (PCRF) is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Validation Project (PCRF) is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Validation Project (PCRF).  If not, see <http://www.gnu.org/licenses/>.
-//
+{
+  Stimulus Control
+  Copyright (C) 2014-2016 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
 unit trial_feature_positive;
 
 {$mode objfpc}{$H+}
