3b5d95ae3e385df9e43473d55a50803f09dd8cdc
pupil <cpicanco@ufpa.br>
2016-08-24 12:02:35 -0300

    add license headers

--- tests/lacs-matrix/form_participant.pas
+++ tests/lacs-matrix/form_participant.pas
@@ -1,3 +1,12 @@
+{
+  Stimulus Control
+  Copyright (C) 2014-2016 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
 unit form_participant;
 
 {$mode objfpc}{$H+}
