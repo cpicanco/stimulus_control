cce6ff38e939d45665d3fd0632d667592865a55f
cpicanco <cpicanco42@gmail.com>
2017-03-12 19:26:01 -0300

    add missing license headers

--- units/config_session_global_container.pas
+++ units/config_session_global_container.pas
@@ -1,3 +1,12 @@
+{
+  Stimulus Control
+  Copyright (C) 2014-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
 unit config_session_global_container;
 
 {$mode objfpc}{$H+}
