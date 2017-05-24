1b2c951cd0000172548b3c1bc6481d9fc32c9d15
cpicanco <cpicanco42@gmail.com>
2017-05-24 12:06:27 -0300

    release linux with static linked libzmq and libbass install instructions

---
+++ docs/installing/linux.md
@@ -0,0 +1,13 @@
+# install libbass.so library
+```
+# open a terminal a navigate to stimulus control folder
+
+# copy the library to system path
+sudo cp libbass.so /usr/local/lib
+
+# give permissions to the library
+sudo chmod a+rx /usr/local/lib/libbass.so
+
+# update linker configurations
+sudo ldconfig
+```
--- releases/release.py
+++ releases/release.py
@@ -28,34 +28,39 @@ def copy_file(src, dst):
 
 RELEASES_FOLDER = 'releases';
 CONFIG_EXAMPLES_FOLDER = os.path.join('docs','config_examples')
-APPLICATION_FILENAME = 'stimulus_control.exe';
+APPLICATION_FILENAME = 'stimulus_control';
 
 if __name__ == "__main__":
     root_path = os.path.dirname(os.path.abspath(__file__))
     root_path = os.path.dirname(root_path)
     print(root_path)
 
-    src_win32_nozmq_files = [
+    src_win32_nozmq_libraries = [
         os.path.join(root_path, 'dependency/lbass/windows/32/bass.dll')
     ]
 
-    src_win32_files = [
+    src_win32_libraries = [
         os.path.join(root_path, 'dependency/lbass/windows/32/bass.dll'),
         os.path.join(root_path, 'dependency/libzmq/win10-32/libzmq.dll')
     ]
 
-    src_win64_files = [
+    src_win64_libraries = [
         os.path.join(root_path, 'dependency/lbass/windows/64/bass.dll'),
         os.path.join(root_path, 'dependency/libzmq/win10-64/libzmq.dll')
     ]
 
+    src_linux64_libraries = [
+        os.path.join(root_path, 'dependency/lbass/linux/64/libbass.so')
+    ]
+
     targets = {
-        'stimulus_control_win10_32bits_nozmq': src_win32_nozmq_files,
-        'stimulus_control_win10_32bits': src_win32_files,
-        'stimulus_control_win10_64bits': src_win64_files
+        'stimulus_control_win10_32bits_nozmq': src_win32_nozmq_libraries,
+        'stimulus_control_win10_32bits': src_win32_libraries,
+        'stimulus_control_win10_64bits': src_win64_libraries,
+        'stimulus_control_linux_64bits': src_linux64_libraries
     }
 
-    for build_name, dllfiles in targets.items(): 
+    for build_name, libraries in targets.items(): 
         # use build name as root for each release
         releases = os.path.join(root_path, RELEASES_FOLDER)
         destination = os.path.join(releases, build_name) 
@@ -66,15 +71,21 @@ if __name__ == "__main__":
             os.makedirs(destination)
 
         # copy build.exe as app_name.exe
-        src = build_name+'.exe'
+        if 'win' in build_name:
+            src = build_name+'.exe'
+            dst = os.path.join(destination, APPLICATION_FILENAME+'.exe')
+
+        if 'linux' in build_name:
+            src = build_name+'.bin'
+            dst = os.path.join(destination, APPLICATION_FILENAME)
+
         src = os.path.join(root_path, src)
-        dst = os.path.join(destination, APPLICATION_FILENAME)
         copy_file(src, dst)
 
         # copy dlls
-        for src_dll in dllfiles:
-            dst_dll = os.path.join(destination,os.path.basename(src_dll))
-            copy_file(src_dll, dst_dll)
+        for src_library in libraries:
+            dst_library = os.path.join(destination,os.path.basename(src_library))
+            copy_file(src_library, dst_library)
 
         # copy config_examples as Participante_1
         src_examples = os.path.join(root_path, CONFIG_EXAMPLES_FOLDER)
