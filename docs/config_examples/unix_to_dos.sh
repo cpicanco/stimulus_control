5ba334ed46ed4b53675b30c806b7f426bfbfde43
cpicanco <cpicanco42@gmail.com>
2017-10-28 11:50:31 -0300

    add exemple of config file with videos

--- docs/config_examples/unix_to_dos.sh
+++ docs/config_examples/unix_to_dos.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+for file in windows*.txt
+do
+    rm $file
+done
+
 for file in linux*.txt
 do
-    from_file=$file 
-    to_file=$(echo $file | cut -f 1 -d '.')
-    # todos unix2dos $file "$name_win.txt"
-    echo $from_file
-    unix2dos -b -u $from_file
+    echo $file
+    unix2dos -b -u $file
+done
+
+for from_file in linux*.txt
+do
+    to_file="${from_file/linux/windows}"
+    mv $from_file $to_file
+    echo $to_file
+done
+
+for from_file in linux*.bak
+do
+    to_file=$(echo $from_file | cut -f 1 -d '.')
+    to_file+=".txt"
+    mv $from_file $to_file
 done
 
 exit
