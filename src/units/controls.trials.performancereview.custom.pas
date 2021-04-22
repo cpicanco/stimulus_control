be0ddf9f75ea253d4890b61c37ad65dae39870f0
Rafael Picanço <cpicanco@users.noreply.github.com>
2021-04-22 04:54:58 -0300

    fix unit name case

--- src/units/controls.trials.performancereview.custom.pas
+++ src/units/controls.trials.performancereview.custom.pas
@@ -7,7 +7,7 @@
   You should have received a copy of the GNU General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
 }
-unit controls.trials.performancereview.custom;
+unit Controls.Trials.Performancereview.Custom;
 
 {$mode objfpc}{$H+}
 
